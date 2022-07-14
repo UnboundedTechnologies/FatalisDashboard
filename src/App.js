8f253f03af761f53d2ddb784a4445949fb598f90
Unbounded Technologies <109149714+UnboundedTechnologies@users.noreply.github.com>
2022-07-14 13:57:29 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,272 @@
+import Spline from "@splinetool/react-spline";
+import styled from "styled-components";
+
+import Logo from "./images/logo.svg";
+import IconTwitter from "./images/icon-twitter.svg";
+import IconYoutube from "./images/icon-youtube.svg";
+import IconLaptop from "./images/icon-laptop.svg";
+
+export default function App() {
+  return (
+    <Wrapper>
+      <TryItNow>
+        <button href="/">Try it now!</button>
+      </TryItNow>
+      <Spline
+        className="spline"
+        scene="https://prod.spline.design/DrWVJhE7LS1x52Go/scene.splinecode"
+      />
+      <Content>
+        <Menu>
+          <li>
+            <img src={Logo} alt="Logo" />
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Download</a>
+          </li>
+          <li>
+            <a href="/">App</a>
+          </li>
+          <li>
+            <a href="/">Login</a>
+          </li>
+        </Menu>
+        <h1>Analyze your data</h1>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt.
+        </p>
+        <button>
+          <img src={IconLaptop} alt="Download" />
+          Download for Windows
+        </button>
+      </Content>
+      <Social>
+        <div />
+        <img src={IconTwitter} alt="Twitter" />
+        <img src={IconYoutube} alt="Youtube" />
+      </Social>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  font-family: "Spline Sans", sans-serif;
+  font-size: 16px;
+  color: white;
+  position: relative;
+  width: 100%;
+  height: 99%;
+  overflow-x: hidden;
+  display: inline-block;
+
+  .spline {
+    margin-left: 10%;
+    margin-top: 3%;
+    right: 0;
+    border-radius: 15px;
+    max-height: 840px;
+
+    @media (max-width: 1024px) {
+      transform: scale(0.8) translateX(200px);
+      transform-origin: top;
+    }
+
+    @media (max-width: 800px) {
+      transform: scale(0.7) translateX(600px);
+    }
+
+    @media (max-width: 600px) {
+      transform: scale(0.5) translateX(-100px);
+      right: auto;
+      left: 50%;
+      margin-left: -600px;
+    }
+
+    @media (max-width: 375px) {
+      transform: scale(0.45) translateX(-50px);
+    }
+  }
+`;
+
+const Content = styled.div`
+  position: absolute;
+  top: 30px;
+
+  display: inline-block;
+  flex-direction: column;
+  gap: 80px;
+
+  @media (max-width: 1024px) {
+    gap: 40px;
+  }
+
+  h1 {
+    font-family: "Spline Sans Mono", monospace;
+    font-weight: bold;
+    font-size: 70px;
+    margin: 0;
+    max-width: 500px;
+    padding-top: 55px;
+
+    @media (max-width: 1024px) {
+      font-size: 60px;
+      max-width: 400px;
+    }
+    @media (max-width: 800px) {
+      font-size: 40px;
+      max-width: 300px;
+    }
+  }
+
+  p {
+    padding-top: 25px;
+    padding-bottom: 25px;
+    font-weight: normal;
+    line-height: 150%;
+    max-width: 380px;
+  }
+
+  button {
+    background: rgba(0, 0, 0, 0.2);
+    border: 0 px;
+    font-size: 16px;
+    padding: 12px 30px;
+    border-radius: 14px;
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    color: white;
+    max-width: 280px;
+    backdrop-filter: blur(20px);
+
+    display: flex;
+    gap: 12px;
+    justify-content: center;
+    align-items: center;
+
+    transition: 1s;
+
+    :hover {
+      border: 1px solid rgba(255, 255, 255, 0.8);
+      transform: translateY(-3px);
+    }
+  }
+
+  h1,
+  p,
+  button {
+    margin: 0 30px 0 100px;
+
+    @media (max-width: 1024px) {
+      margin: 0 30px;
+    }
+  }
+`;
+
+const Menu = styled.ul`
+  display: flex;
+  gap: 30px;
+  align-items: center;
+  margin: 0 30px 0 100px;
+  padding: 0;
+
+  @media (max-width: 1024px) {
+    margin: 0 30px;
+  }
+
+  li {
+    list-style: none;
+    margin: 0;
+
+    a {
+      text-decoration: none;
+      color: white;
+      padding: 8px 20px;
+      border: 1px solid rgba(255, 255, 255, 0);
+      transition: 1s;
+      border-radius: 14px;
+
+      :hover {
+        border: 1px solid rgba(255, 255, 255, 0.2);
+      }
+    }
+  }
+
+  @media (max-width: 800px) {
+    justify-content: space-between;
+    li:nth-child(2),
+    li:nth-child(3),
+    li:nth-child(4),
+    li:nth-child(5) {
+      display: none;
+    }
+  }
+`;
+
+const Social = styled.div`
+  position: absolute;
+  top: 150px;
+  left: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  align-items: center;
+
+  div {
+    width: 1px;
+    height: 500px;
+
+    background: linear-gradient(
+      180deg,
+      #08b6f9 0%,
+      #6c56ef 33.57%,
+      #1306dd 65.86%,
+      #aa0eb2 100%
+    );
+  }
+
+  @media (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+const TryItNow = styled.div`
+  float: right;
+  align-items: center;
+  margin-right: 20px;
+  margin-top: 35px;
+
+  button {
+    background: rgba(0, 0, 0, 0.2);
+    border: 0 px;
+    font-size: 16px;
+    padding: 12px 30px;
+    border-radius: 14px;
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    color: white;
+    max-width: 280px;
+    backdrop-filter: blur(20px);
+
+    display: flex;
+    gap: 12px;
+    justify-content: center;
+    align-items: center;
+
+    transition: 1s;
+
+    :hover {
+      border: 1px solid rgba(255, 255, 255, 0.8);
+      transform: translateY(-3px);
+    }
+
+    @media (max-width: 1024px) {
+      margin: 0 30px;
+    }
+  }
+`;
